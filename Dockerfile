631cbfee6ce714740d6c246da827cc7cc2d346d8
yangbo568969859 <568969859@qq.com>
2020-08-12 13:46:25 +0800

    merge

--- Dockerfile
+++ Dockerfile
@@ -24,3 +24,6 @@ FROM nginx
 # RUN rm /etc/nginx/conf.d/nginx.conf
 COPY --from=build-stage /app/middle_data_analysis/ /usr/share/nginx/html/
 COPY nginx.conf /etc/nginx/conf.d/default.conf
+
+EXPOSE 80
+CMD ["nginx", "-g", "daemon off;"]
--- cron.sh
+++ cron.sh
@@ -4,9 +4,9 @@ echo $image_version;
 git pull --rebase origin master
 docker stop vue-docker1;
 docker rm vue-docker1;
-docker build -t vue-docker1 .;
+docker build -t vue-docker1:$image_version .;
 docker images;
-docker run -p 3000:80 -d --name vue-docker1 vue-docker1;
+docker run -p 3000:80 -d --name vue-docker1 vue-docker1:$image_version;
 docker logs vue-docker1;
 #删除build过程中产生的镜像    #docker image prune -a -f
 docker rmi $(docker images -f "dangling=true" -q)
